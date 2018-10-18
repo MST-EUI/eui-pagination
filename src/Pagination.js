229f845a9c48fb56240457cd0047a774837b4c78
yukap <jingweirong@mistong.com>
2018-10-18 14:27:19 +0800

    feat: add prop-types

--- src/Pagination.js
+++ src/Pagination.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import RcPagination from 'rc-pagination';
 import classnames from 'classnames';
-// import i18n from './i18n';
+import PropTypes from 'prop-types';
 
 import './style/index.scss';
-
-const { PropTypes } = React;
 
 export default class Pagination extends React.Component {
   static propTypes = {
